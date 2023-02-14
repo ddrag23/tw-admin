441a2d246b117d7635c1bdc8d23289f99a6dd1be
alwi-floo <alwi@floo.id>
2023-02-14 14:26:24 +0700

    add padding in title menu

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -11,7 +11,7 @@ function subIsActive(name: string) {
 <template>
   <section class="w-1/5 bg-white p-5 h-screen hidden md:block overflow-auto">
     <h1 class="text-2xl font-bold text-center mt-5 mb-12">TheDashboard</h1>
-    <p class="text-gray-400 mb-5">Main Menu</p>
+    <p class="px-2 text-gray-400 mb-3">Main Menu</p>
     <nav class="w-full flex flex-col gap-2">
       <template v-for="(item, index) in mainMenu" :key="index">
         <RouterLink
@@ -25,14 +25,6 @@ function subIsActive(name: string) {
           <span class="ml-2">{{ item.title }}</span>
         </RouterLink>
       </template>
-      <!-- <RouterLink to="/user" class="p-2 text-slate-400">
-        <font-awesome-icon icon="fa-solid fa-users"></font-awesome-icon>
-        <span class="ml-2">User</span>
-      </RouterLink>
-      <RouterLink to="/transaksi" class="p-2 text-slate-400">
-        <font-awesome-icon icon="fa-solid fa-money-bill" />
-        <span class="ml-2">Transaksi</span>
-      </RouterLink> -->
     </nav>
   </section>
 </template>
